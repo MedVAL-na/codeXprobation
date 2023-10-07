68fa9cf2ef8908167911ff3eb445201d470662b7
Valentina <v.m.30.03.20@gmail.com>
2023-10-07 18:45:13 +0300

    added icons, storybook works correctly

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -3,6 +3,53 @@ import './button.css';
 export default {
     title: 'Button',
     component: Button,
+    argTypes: {
+        type:{
+            type: 'string',
+            description: 'Set visual type of a button',
+            defaultValue: 'solid',
+            options: ['solid', 'outline', 'soft', 'ghost'],
+            control: {
+                type: 'radio',
+            }
+        },
+        size:{
+            type: 'string',
+            description: 'Set size of a button',
+            defaultValue: 'medium',
+            options: ['small', 'medium', 'large'],
+            control: {
+                type: 'radio',
+            }
+        },
+        style:{
+            type: 'string',
+            description: 'Set visual style of a button',
+            defaultValue: 'primary',
+            options: ['primary', 'secondary', 'danger'],
+            control: {
+                type: 'radio',
+            }
+        },
+        iconType:{
+            type: 'string',
+            description: 'Affects button paddings',
+            defaultValue: 'none',
+            options: ['none', 'leading', 'trailing', 'leadingTrailing', 'standalone'],
+            control: {
+                type: 'radio',
+            }
+        },
+        state:{
+            type: 'string',
+            description: 'Set current state of a button',
+            defaultValue: 'solid',
+            options: ['default', 'hover', 'disabled'],
+            control: {
+                type: 'radio',
+            }
+        }
+    },
 }
 const Template = (arg) => <Button{...arg}/>
 export const Default = Template.bind({});
@@ -12,6 +59,18 @@ Default.args = {
     type : 'solid',
     size : 'medium',
     style : 'primary',
+    iconType : 'none',
+    state : 'default',
+
+};
+
+export const Large = Template.bind({});
+
+
+Large.args = {
+    type : 'solid',
+    size : 'large',
+    style : 'danger',
     iconType : 'none',
     state : 'default',
 
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './button.css';
 
+import { IconBrackets, IconPlus  } from '@codexteam/icons';
+
 interface ButtonProps {
   type?: 'solid'| 'outline'| 'soft' | 'ghost';
   size?: 'small' | 'medium' | 'large';
@@ -22,15 +24,14 @@ export const Button = ({
       <button
         type="button"
         className={['button',  `button-size-${size}`, `button-style-${style}`,
-        `button-type-${type}`, `button-style-${iconType}`, `button-state-${state}`,].join(' ')}
+        `button-type-${type}`, `button-iconType-${iconType}`, `button-state-${state}`,].join(' ')}
       >
-        Button
-        <style jsx>{`
-          button {
-           
-          }
-        `}</style>
-      </button>
+        {iconType == "none" ? <div>Button</div> 
+        :iconType == "leading" ? <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 7V12M12 17V12M17 12H12M12 12H7"/></svg> {"Button"}</span> 
+        : iconType == "trailing" ? <span>{"Button"} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8L5 12L9 16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 8L19 12L15 16"/></svg></span> :
+        iconType == "leadingTrailing" ? <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 7V12M12 17V12M17 12H12M12 12H7"/></svg> {"Button"} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8L5 12L9 16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 8L19 12L15 16"/></svg></span> 
+        :<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 7V12M12 17V12M17 12H12M12 12H7"/></svg>}
+        </button>
     );
   };
   
