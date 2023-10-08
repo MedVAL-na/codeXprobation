a33738223c4958e7516dc8029bfe63587162a136
Valentina <v.m.30.03.20@gmail.com>
2023-10-08 14:38:22 +0300

    added type & state of button

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './button.css';
 
-import { IconBrackets, IconPlus  } from '@codexteam/icons';
-
 interface ButtonProps {
   type?: 'solid'| 'outline'| 'soft' | 'ghost';
   size?: 'small' | 'medium' | 'large';
@@ -27,10 +25,14 @@ export const Button = ({
         `button-type-${type}`, `button-iconType-${iconType}`, `button-state-${state}`,].join(' ')}
       >
         {iconType == "none" ? <div>Button</div> 
-        :iconType == "leading" ? <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 7V12M12 17V12M17 12H12M12 12H7"/></svg> {"Button"}</span> 
-        : iconType == "trailing" ? <span>{"Button"} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" transform='rotate(90)' fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8L5 12L9 16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 8L19 12L15 16"/></svg></span> :
-        iconType == "leadingTrailing" ? <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 7V12M12 17V12M17 12H12M12 12H7"/></svg> {"Button"} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" transform='rotate(90)' fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8L5 12L9 16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 8L19 12L15 16"/></svg></span> 
-        :<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 7V12M12 17V12M17 12H12M12 12H7"/></svg>}
+        :iconType == "leading" ? <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -4 20 20" fill="none"><path stroke="currentColor"  d="M10 5V10M10 15V10M15 10H10M10 10H5" strokeWidth="1.6" strokeLinecap="round"/>
+        </svg>{"Button"}</span> 
+        : iconType == "trailing" ? <span>{"Button"} <svg xmlns="http://www.w3.org/2000/svg"  width="15" height="15" transform='rotate(90)' fill="none" viewBox="0 1 20 20"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 8L5 12L9 16"/><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 8L19 12L15 16"/></svg></span> :
+        iconType == "leadingTrailing" ? <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -4 20 20" fill="none"><path d="M10 5V10M10 15V10M15 10H10M10 10H5" stroke="currentColor"  strokeWidth="1.6" strokeLinecap="round"/>
+        </svg>{"Button"} <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" transform='rotate(90)' fill="none" viewBox="0 1 20 20"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 8L5 12L9 16"/><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 8L19 12L15 16"/></svg></span> 
+        :<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <path d="M10 5V10M10 15V10M15 10H10M10 10H5" stroke="currentColor"  strokeWidth="1.6" strokeLinecap="round"/>
+        </svg>}
         </button>
     );
   };
